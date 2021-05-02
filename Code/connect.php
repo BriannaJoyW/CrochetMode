bd7664608d663c79badbcc4bcfc6f732f5d7128f
BriannaJoyW <66181252+BriannaJoyW@users.noreply.github.com>
2021-05-02 15:26:27 -0400

    Delete connect.php

--- Code/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "crochet123";
-$dbName = "CrochetMode";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
